df343c6c9e2b02598b2b0f41fb0c60f3b59c3587
woni928 <398401414@qq.com>
2020-04-01 13:33:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic.git
+git clone https://github.com/Ameykyl/luci-app-koolproxyR package/luci-app-koolproxyR
+./scripts/feeds update -a
+./scripts/feeds install -a
